757c912d2a83e603fc6a980df5efacb8b885de49
levgeogr <levgeogr@gmail.com>
2017-10-22 15:00:35 +0300

    welcome modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    КАФЕДРА ГИСиТ
                 </div>
 
                 <div class="links">
