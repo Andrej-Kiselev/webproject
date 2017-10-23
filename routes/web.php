3cd6f1eb433ad912623d34e9abd9b12b3412ed8c
Andrej_Kiselev <andrej.kiselev73@yandex.ru>
2017-10-23 13:07:47 +0300

    Маленькие изменения.

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,6 @@
 */
 
 Route::get('/', 'mainPageController@index');
-//Route::get('main', 'mainPageController@index');
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/about', 'AboutController@index')->name('about');
+Auth::routes();
