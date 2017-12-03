5a2505d8826e769a4615c907c72ec0541a9626da
Natalia Minogina <natalia.minogina@gmail.com>
2017-12-03 22:15:24 +0300

    Âîçìîæíîñòü äîáàâëåíèÿ èçäàòåëüñòâà â àäìèíêå

--- resources/views/admin/admin_authorization.blade.php
+++ resources/views/admin/admin_authorization.blade.php
@@ -33,7 +33,7 @@
       </div>
   </form>
   <!-- Вывод всех пользователей -->
-  <div style="float: right; width: 51%;">
+  <div style="float: right; width: 51%; height: 300px;">
     <table>
       <tr id="header">
         <td>Пользователь</td>
@@ -51,5 +51,31 @@
       </tr>
     </table>
   </div>
+</div>
+<hr>
+<div>
+  <div style="float: left; width: 45%; margin-top: 10px;">
+  <form role="form" method="POST" action="/adduser">
+    <div class="form-group" >
+        <textarea class="form-control" placeholder="Издательство" name="name_of_publishing_house" rows="1"></textarea>
+    </div>
+    <input type="submit" class="btn btn-lg btn-info btn-block" value="Добавить издательство">
+  </form>
+</div>
+<div style="float: right; width: 51%; height: 400px;">
+  <table>
+    <tr id="header">
+      <td>Издательство</td><td></td>
+    </tr>
+    <tr>
+      <td>Автор Автор Автор Автор</td>
+      <td><a href="#"><span class="glyphicon glyphicon-trash"></span></a></td>
+    </tr>
+    <tr>
+      <td>Автор Автор Автор Автор</td>
+      <td><a href="#"><span class="glyphicon glyphicon-trash"></span></a></td>
+    </tr>
+  </table>
+</div>
 </div>
 @stop
